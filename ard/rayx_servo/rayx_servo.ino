7fc60011465864ddc1fee694792aa51787c58c9d
Photon <1487quantum@users.noreply.github.com>
2017-12-28 17:25:53 +0800

    Update rayx_servo.ino
    Reorganised code

--- ard/rayx_servo/rayx_servo.ino
+++ ard/rayx_servo/rayx_servo.ino
@@ -1,77 +1,97 @@
 #include <Servo.h>
-
+#define DEBUG 1
 //Var
-int alp = 80;           //Max Range
-float amp = 0;            //Final max range
-int f = 1;             //Frequency
-float phi = 0;       //Phase offet
-float t = 0;              //Time t
+int alp = 80;             //Max Range
+float amp = 0;            //Constrained max range
+int f = 1;                //Frequency
 
-float b = 0;              //Center offset
-bool k = 0;             //Motion Mode (0:straight, 1:inclined)
-bool d = 0;             //Direction (0: Straight (1), 1: Backwards (-1))
-float g = 0.00;         //Inclination coefficient
+bool k = 0;               //Motion Mode (0:straight, 1:inclined)
+bool d = 0;               //Direction (0: Straight (1), 1: Backwards (-1))
+float g = 0.00;           //Inclination coefficient
 
 float out = 0;            //Servo output
 
+float percentage;
+
 //Servo
-const int sMax = 80;    //Maximum servo range
-Servo servObj0;  // create servo object to control a servo
-Servo servObj1;  // create servo object to control a servo
-Servo servObj2;  // create servo object to control a servo
+#define NUM_SERVOS 3
+#define SMAX 80           //Maximum servo range
+
+//Left
+Servo servObj0;
+Servo servObj1;
+Servo servObj2;
+//Right
+Servo servObj3;
+Servo servObj4;
+Servo servObj5;
 
 Servo servosL[] = {servObj0, servObj1, servObj2};
-#define NUM_SERVOS (sizeof(servosL) / sizeof(Servo))
+//Servo servosR[] = {servObj3, servObj4, servObj5};
 
-int pos = 0;    // variable to store the servo position
+//#define NUM_SERVOS (sizeof(servosL) / sizeof(Servo))
+
+
+void setParams(int a, int frq, bool dir, bool m_mode, float inc) {
+  alp = a > 0 ? a : 80;   //Amplitude: if a >0: set a, else default 80
+  f = frq > 0 ? frq : 1;  //Freq: if frq >0: set frq, else default 1
+  d = dir;
+  k = m_mode;
+  g = k ? inc : 1; //if k==1: incline, else straight
+}
+
+float calCAmp() {
+  //Get constrained amplitude
+  return constrain((float) alp / (1 + g * k), 0, SMAX);
+}
+
+float getPercent() {
+  float t = micros() % 1000000;  // time
+  return t / 1000000;   //Percent of sine
+}
+
+//Phase offet -> index k, sNum: NUM_SERVOS
+float calPhi(int k, int sNum) {
+  return 2 * PI * (k / (float) sNum);
+}
+
+//Calculate center offset -> j:index, dr:dir
+float calCenter(int j, int dr) {
+  return g * (alp / 2) * (1 - j) * (dr * k);
+}
+
+//pc->percentage, j->srv indx, dir->d
+void servoOut(float pc, int j, int d) {
+  out =  amp * sin( 2 * PI * (pc) * f + calPhi(j, NUM_SERVOS)) + calCenter(j, d);
+  if (DEBUG) {
+    Serial.print(out);
+    if (j < NUM_SERVOS - 1)  Serial.print(",");
+  }
+  servosL[j].write((int)out + 90);  //Add 90 to output (Center is 90)
+}
 
 void setup() {
-  alp = 25;
-  f=1;
-  k = 0;
-  if (k) {
-    //Only used when in incline mode
-    g = 1;
-    d = 0;
-  }
-  //Initialise servo
+  setParams(25, 1, 0, 1, 1);
+  //Initialise servos
   for (int j = 9; j < 12; j++) {
     servosL[j - 9].attach(j);
     servosL[j - 9].write(90);
     delay(1000);
   }
   delay(1000);  //Wait for 1s
-  //Get final amplitude
-  amp = (float) alp / (1 + g * k);
-  amp = constrain(amp, 0, sMax);  //Constrain final amplitude
-  Serial.begin(115200);
+  amp = calCAmp();
+  if (DEBUG) {
+    Serial.begin(115200);
+  }
 }
 
 void loop() {
-  t = micros() % 1000000;
-  float percentage = t / 1000000;   //Percent of sine
-
-  for (int i = 0; i < NUM_SERVOS; i++) {
-    phi =  2 * PI * (i / (float)NUM_SERVOS);
-    // Serial.print(phi);
-    b = g * (alp / 2) * (1 - i) * ((d == 0 ? 1 : -1) * k) ;
-    // Serial.print(b);
-    out = amp * sin( 2 * PI * (percentage) * f + phi) + b;
-    Serial.print(out);
-
-    if (i < NUM_SERVOS - 1)  Serial.print(",");
-
-    //Add 90 to output
-    servosL[i].write((int)out + 90);
-
-    /*
-      float time = micros() % 1000000;
-      float percentage = time / 1000000;
-      float templitude = sin(((percentage) * freq) * 2 * PI);
-     * */
-
+  percentage = getPercent();
+  for (int i = 0; i < NUM_SERVOS; i++) {  //D bool -> dir int
+    servoOut(percentage, i, (d == 0 ? 1 : -1));
   }
-  Serial.println(" ");
+  if (DEBUG) {
+    Serial.println(" ");
+  }
   delay(1);
 }
-
